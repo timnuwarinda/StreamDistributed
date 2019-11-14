e436e619e4adda39d550a959e3b5b7dcf11e0b60
Timothy <hiitu7even@gmail.com>
2019-11-14 23:40:46 +0300

    server loadbalancers

--- client/src/app/stream/livestreams/livestreams.component.ts
+++ client/src/app/stream/livestreams/livestreams.component.ts
@@ -35,7 +35,7 @@ export class LivestreamsComponent implements OnInit , OnChanges, AfterContentChe
   
   }
   getLiveStreams() {
-    axios.get('http://35.224.91.202:' + config.rtmp_server.http.port + '/api/streams')
+    axios.get('http://34.67.57.77:' + config.rtmp_server.http.port + '/api/streams')
         .then(res => {
             let streams = res.data;
             // console.log(`res data:`, streams['live']);
@@ -45,8 +45,8 @@ export class LivestreamsComponent implements OnInit , OnChanges, AfterContentChe
         });
   }
   getStreamsInfo(live_streams) {
-    axios.get('http://35.224.91.202:3333/streams/info', {
-      // axios.get('http://35.224.91.202:' + config.rtmp_server.http.port  + '/streams', {
+    axios.get('http://34.67.57.77:3333/streams/info', {
+      // axios.get('http://34.67.57.77:' + config.rtmp_server.http.port  + '/streams', {
 
         params: {
             streams: live_streams
